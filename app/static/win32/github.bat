a7a44c4c0c4c60c5667c208f7dd237aa58310096
William Shepherd <iamwillshepherd@github.com>
2017-07-11 22:03:17 -0500

    Check if dev build exists

--- app/static/win32/github.bat
+++ app/static/win32/github.bat
@@ -2,6 +2,11 @@
 setlocal
 
 set ELECTRON_RUN_AS_NODE=1
-call "%~dp0..\..\..\GitHub Desktop.exe" "%~dp0..\cli.js" %*
+
+if exists "%~dp0..\..\..\GitHub Desktop.exe" (
+	call "%~dp0..\..\..\GitHub Desktop.exe" "%~dp0..\cli.js" %*
+) else (
+	call "%~dp0..\..\..\GitHub Desktop-dev.exe" "%~dp0..\cli.js" %*
+)
 
 endlocal
